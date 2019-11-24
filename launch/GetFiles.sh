e059e8ba6275d19639e53ceca6164f3f8a0fe046
kieranstrobel <46137709+kieranstrobel@users.noreply.github.com>
2019-11-24 15:06:25 -0500

    Delete GetFiles.sh

--- launch/GetFiles.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ~/DUT18D_ws/src/cv/tensorrt_ros/launch/calib_imgs
-cd ~/DUT18D_ws/src/cv/tensorrt_ros/launch/calib_imgs 
-gsutil -m cp -p gs://mit-dut-driverless-external/tiling_dataset_non_square/* .
